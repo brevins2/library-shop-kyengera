186070f62f26a86bd42860909a1001add0d52892
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-05-07 04:42:03 +0300

    up to date

--- src/app/Page/add-phone/add-phone.component.ts
+++ src/app/Page/add-phone/add-phone.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-phone',
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AddPhoneComponent implements OnInit {
 
-  selectedFile!: File;
+  selectedFile: File | null = null; 
   constructor(private http: HttpClient,private router: Router) { }
 
   ngOnInit(): void {
@@ -28,16 +28,25 @@ export class AddPhoneComponent implements OnInit {
   }
 
   onFileUpload(event: any){
-    this.selectedFile = event.target.files[0];
+    this.selectedFile = <File>event.target.files[0];
   }
 
   onUpload(){
-    const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-    this.http.post("http://localhost/3000/Phones", fd).subscribe(response => {
-      console.log(response);
+    if(this.selectedFile != null){
+      const fd = new FormData();
+      fd.append('image', this.selectedFile, this.selectedFile.name);
+      this.http.post("http://localhost/3000/Phones", fd, {reportProgress: true,
+      observe: 'events'}).subscribe(event => {
+        if(event.type === HttpEventType.UploadProgress){
+          // console.log('Upload Progress: '+ Math.round(event.loaded / event.total * 100));
+          console.log('done');
+        }
+        else if(event.type === HttpEventType.Response){
+          console.log(event);
+        }
     });
   }
+}
 
   save(){
     console.warn(this.addPhone.value);
