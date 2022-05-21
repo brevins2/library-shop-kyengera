c34adf05bd646c48f1b170081a4d669e5a24c785
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-05-21 23:29:27 +0300

    initial commit

--- src/app/Page/admin/admin.component.ts
+++ src/app/Page/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
 
@@ -11,27 +10,11 @@ import { Router } from '@angular/router';
 })
 export class AdminComponent implements OnInit {
 
-   @ViewChild(MatSidenav) sidenav!: MatSidenav;
-   constructor(
-     private observer: BreakpointObserver,
-     private router: Router
-   ) { }
+  showFiller = false;
+   constructor(private router: Router) { }
 
 
   ngOnInit(): void {
-  }
-
-  ngAfterViewInit(){
-    this.observer.observe(['(max-width: 500px)']).subscribe((res)=>{
-      if(res.matches){
-        this.sidenav.mode = 'side';
-        this.sidenav.close();
-      }
-      else{
-        this.sidenav.mode = 'over';
-        this.sidenav.open();
-      }
-    });
   }
 
   search(){}
--- src/app/Page/user/slider.js
+++ src/app/Page/user/slider.js
@@ -1,16 +1,20 @@
-const searchBar = document.forms['searchBarId'].querySelector('searched');
+// const searchBar = document.forms['searchBarId'].querySelector('searched');
 
-searchBar.addEventListener('keyup', function(e) {
-    const searchedInput = e.target.value.toLowerCase();
-    const divPhones = list.getElementsByTagName('div');
+// searchBar.addEventListener('keyup', function(e) {
+//     const searchedInput = e.target.value.toLowerCase();
+//     const divPhones = list.getElementsByTagName('div');
 
-    Array.from(divPhones).forEach(function(divPhone) {
-        const title = divPhone.firstElementChild.textContent;
+//     Array.from(divPhones).forEach(function(divPhone) {
+//         const title = divPhone.firstElementChild.textContent;
 
-        if (title.toLowerCase().indexOf(term) != -1) {
-            divPhone.style.display = 'block';
-        } else {
-            divPhone.style.display = 'none'
-        }
-    })
+//         if (title.toLowerCase().indexOf(term) != -1) {
+//             divPhone.style.display = 'block';
+//         } else {
+//             divPhone.style.display = 'none'
+//         }
+//     })
+// });
+
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus')
 })
