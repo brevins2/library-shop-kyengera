07838f125b3c33b80e9579426404f394a107d92e
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-05-31 01:02:34 +0300

    first page

--- src/app/Component/slider/slider.component.ts
+++ src/app/Component/slider/slider.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatDialog } from '@angular/material/dialog';
-import { PhonePayComponent } from 'src/app/Pay/phone-pay/phone-pay.component';
 import { ActivatedRoute } from '@angular/router';
 import { ServeService } from 'src/app/Services/serve.service';
+import { ComputerPayComponent } from 'src/app/Pay/computer-pay/computer-pay.component';
+import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 export class Api{
   constructor(
@@ -13,7 +14,8 @@ export class Api{
     public Battery: string,
     public Price: number,
     public File: string,
-    public Brand: string
+    public Brand: string,
+    public Category: string
   ){}
 }
 
@@ -25,15 +27,25 @@ export class Api{
 export class SliderComponent implements OnInit {
 
   api: Api[] = [];
+  comp: Api[] = [];
+  public sendMessage!: FormGroup;
+  alerts = false;
   constructor(
     private http: HttpClient,
-    private dialogRef: MatDialog,
     private router: ActivatedRoute,
-    private service: ServeService
+    private service: ServeService,
+    private dialogRef: MatDialog,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.getApi();
+    this.getApiComputer();
+    this.sendMessage = this.formBuilder.group({
+      email: [''],
+      name: [''],
+      message: ['']
+    });
   }
   getCurrentDatas(){
     this.service.getCurrentPhoneData(this.router.snapshot.params['id']).subscribe((results)=>{
@@ -47,5 +59,28 @@ export class SliderComponent implements OnInit {
       response=>{
         this.api = response
       });
+  }
+
+  // computers
+  watch(){
+    this.dialogRef.open(ComputerPayComponent);
+  }
+  
+  getApiComputer(){
+    this.http.get<any>('http://localhost:3000/Computers').subscribe( response=>
+      {
+        this.comp = response;
+      });
+  }
+
+  // contact information
+  sendMessages(){
+    this.http.post<any>('http://localhost:3000/Message', this.sendMessage.value).subscribe(res=>{
+      // this.sendMessage = res;
+      this.sendMessage.reset();
+    },
+    error=>{
+      this.alerts =true;
+    });
   }
 }
