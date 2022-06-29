55fa7376db5d4216a53793f6e3f1eb32765eb66c
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-06-29 10:24:07 +0300

    express, node and mysql added, backend system

---
+++ src/app/controllers/tutorial.controller.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// create and Save a new Tutorial
+exports.findAll = (req, res) => {
+};
+
+//Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+
+};
+
+// update a Tutorials from the database.
+exports.update = (req, res) => {
+
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+};
