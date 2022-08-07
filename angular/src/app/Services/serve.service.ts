cf1f7cfe5cb837c6d465510014665504cb6bd7d8
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-08-07 12:21:05 +0300

    update

--- angular/src/app/Services/serve.service.ts
+++ angular/src/app/Services/serve.service.ts
@@ -126,7 +126,7 @@ export class ServeService {
     }
 
    // for image uploads
-   private baseUrl = "http://localhost:3000/upload"
+   private baseUrl = "http://localhost:3000"
    upload(file: File): Observable<HttpEvent<any>> {
        const formData: FormData = new FormData();
        formData.append('file', file);
@@ -140,3 +140,4 @@ export class ServeService {
        return this.http.get(`${this.baseUrl}/files`);
     }
 }
+ 
