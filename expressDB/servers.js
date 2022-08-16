b38e3fac2077b41f4473df2541bd149d84d568d5
Kiggundu Brevin <kiggundubrevin99@gmail.com>
2022-08-16 23:21:48 +0300

    'back-end'

--- expressDB/servers.js
+++ expressDB/servers.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 
 // database connection
@@ -22,7 +22,7 @@ const db = mysql.createConnection({
 db.connect(err => {
       if(err){ console.log('err'); }
         console.log('database connected....');
-});
+})
 
 
 //get all the data
@@ -46,7 +46,81 @@ app.get('/Message', (req, res) =>{
     });
 });
 
+// get single data
 
+app.get('/Message/:id', (req, res) =>{
+
+    let gID = req.params.id;
+
+    let qr =`select * from messages where id = ${gID}`;
+
+    db.query(qr, (err, results) =>{
+
+        if(err)
+        {
+            console.log(err);
+        }
+        if(results.length > 0)
+        {
+            res.send({
+                Message: 'getting single data',
+                data: results
+            });
+        }
+        else{
+            res.send({
+                message: 'data not found'
+            });
+        }
+    });
+});
+
+
+app.post('/add/Message', (req, res) =>{
+
+    console.log(req.body ,'data added');
+
+    let email = req.body.email;
+    let name = req.body.name;
+    let message = req.body.message;
+
+    let qr = `INSERT INTO messages(email, name, message) VALUES('${email}','${name}','${message}')`;
+
+    db.query(qr, (err, result) =>{
+
+        if(err){ console.log(err); }
+
+           res.send({
+               message: 'data sent successfully'
+           });
+
+    });
+});
+
+
+// put data/ update data
+
+app.put('/update/Message/:id', (req, res) =>{
+
+    console.log(req.body ,'data updated');
+
+    let gID = req.params.id;
+    let email = req.body.email;
+    let name = req.body.name;
+    let message = req.body.message;
+
+    let qr = `update messages set email = '${email}', name = '${name}', message = '${message}' where id = ${gID}`;
+
+    db.query(qr, (err, result) =>{
+        if(err) { console.log(err); }
+
+        res.send({
+            message: 'data successfully updated'
+        });
+    });
+
+
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
